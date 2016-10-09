b6d2812fe5ff1228c62edda22e405f4bbd97c145
Micheal Yin <layerssss@gmail.com>
2016-10-09 19:47:05 +1300

    Use transaction

--- app/models/dojo.rb
+++ app/models/dojo.rb
@@ -39,24 +39,26 @@ class Dojo < ApplicationRecord
   end
 
   def player_perform!(player, action)
-    begin
-      raise "Too slow! Not your turn..." unless player.id == active_player_id
-      player.perform! action
-    rescue RuntimeError => e
-      puts e.message
-      PlayerChannel.broadcast_to(player, error: e.message)
+    transaction do
+      begin
+        raise "Too slow! Not your turn..." unless player.id == active_player_id
+        player.perform! action
+      rescue RuntimeError => e
+        puts e.message
+        PlayerChannel.broadcast_to(player, error: e.message)
+      end
+      players.dead.each do |player|
+        next if player.died_at > 10.seconds.ago
+        player.update_attributes!(alive: true)
+      end
+      DojoChannel.broadcast_to(
+        self,
+        dojo: self,
+        players: players,
+        ammos: ammos,
+      )
+      sleep 1 unless fast?
     end
-    players.dead.each do |player|
-      next if player.died_at > 10.seconds.ago
-      player.update_attributes!(alive: true)
-    end
-    DojoChannel.broadcast_to(
-      self,
-      dojo: self,
-      players: players,
-      ammos: ammos,
-    )
-    sleep 1 unless fast?
     next_player!
   end
 
@@ -65,28 +67,30 @@ class Dojo < ApplicationRecord
   end
 
   def next_player!
-    reload
-    new_active_player = nil
-    if active_player.nil? || !active_player.alive
-      new_active_player = players.alive.first
-    else
-      index = players.alive.find_index(active_player)
-      new_active_player =  players.alive[(index + 1) % players.alive.size]
+    transaction do
+      reload
+      new_active_player = nil
+      if active_player.nil? || !active_player.alive
+        new_active_player = players.alive.first
+      else
+        index = players.alive.find_index(active_player)
+        new_active_player =  players.alive[(index + 1) % players.alive.size]
+      end
+      update_attributes! active_player: new_active_player, active_player_updated_at: Time.current
+      # TODO: refactor that
+      ammos.create! unless ammos.size > width * height * 0.1
+      if new_active_player
+        PlayerChannel.broadcast_to(
+          new_active_player,
+          step: {
+            dojo: self,
+            player: new_active_player,
+            enemies: players.alive.reject{ |p| p.id == new_active_player.id },
+            ammos: ammos,
+          }
+        )
+      end
+      new_active_player
     end
-    update_attributes! active_player: new_active_player, active_player_updated_at: Time.current
-    # TODO: refactor that
-    ammos.create! unless ammos.size > width * height * 0.1
-    if new_active_player
-      PlayerChannel.broadcast_to(
-        new_active_player,
-        step: {
-          dojo: self,
-          player: new_active_player,
-          enemies: players.alive.reject{ |p| p.id == new_active_player.id },
-          ammos: ammos,
-        }
-      )
-    end
-    new_active_player
   end
 end
