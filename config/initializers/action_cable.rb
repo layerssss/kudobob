7a234d3892773d5794475126320c2ccbfca39e6d
Micheal Yin <layerssss@gmail.com>
2017-05-07 18:47:25 +1200

    Setup ssl

--- config/initializers/action_cable.rb
+++ config/initializers/action_cable.rb
@@ -1,2 +1,2 @@
 #Rails.application.config.action_cable.url = 'wss://example.com/cable'
-Rails.application.config.action_cable.allowed_request_origins = [ "http://#{ENV['RAILS_APP_HOST']}" ]
+Rails.application.config.action_cable.allowed_request_origins = [ "https://#{ENV['RAILS_APP_HOST']}" ]
